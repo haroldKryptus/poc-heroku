d642bf11e8dfd2edac5d94dc285f29fadd62d0b4
haroldKryptus <49311820+haroldKryptus@users.noreply.github.com>
2020-08-12 14:25:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php
-
-echo 'Hello World';
-
-?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Kryptus</title>
+</head>
+<body>
+<img src="https://kryptus.com/wp-content/uploads/2017/11/logo-cin.png">
+</body>
+</html>
